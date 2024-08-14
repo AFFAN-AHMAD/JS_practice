1bc8b2f9e30cdfebddedfcc1d442451e0227556e
affan <affan.ahmad@superprocure.com>
2024-08-14 11:18:11 +0530

    14 Aug 2024: used AbortController to solve the fetchWithTimeout problem

---
+++ js practice/fetchWithTimeOut/prashantYadavSolution.js
@@ -0,0 +1,38 @@
+const fetchWithTimeout = (url, duration) => {
+  return new Promise((resolve, reject) => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    let timerId = null;
+    fetch(url, { signal })
+      .then((resp) => {
+        resp
+          .json()
+          .then((result) => {
+            clearTimeout(timerId);
+            resolve(result);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    timerId = setTimeout(() => {
+      console.log("Aborted");
+      controller.abort();
+      reject(new Error("Time limit exceed"));
+    }, duration);
+  });
+};
+
+fetchWithTimeout("https://jsonplaceholder.typicode.com/todos/1", 10)
+  .then((resp) => {
+    console.log(resp);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Aborted
+// error
