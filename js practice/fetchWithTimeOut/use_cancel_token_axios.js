137eb714d96dc23f0c8a7493f5d04777d44bf687
affan <affan.ahmad@superprocure.com>
2024-08-17 00:01:03 +0530

    16 aug: cancelled request using axios cancel toke

--- js practice/fetchWithTimeOut/use_cancel_token_axios.js
+++ js practice/fetchWithTimeOut/use_cancel_token_axios.js
@@ -1,1 +1,36 @@
-import axios from axios;
+const axios = require('axios')
+const fetchWithTimeout = (url, duration) => {
+  return new Promise((resolve, reject) => {
+    const cancelTokenSource = axios.CancelToken.source();
+    console.log('cancelTokenSource', cancelTokenSource);
+    let timerId = null;
+    axios.get(url, { cancelToken: cancelTokenSource.token })
+      .then((resp) => {
+        // console.log("resp", resp)
+
+        clearTimeout(timerId);
+        resolve(resp.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+    timerId = setTimeout(() => {
+      console.log("Aborted");
+      cancelTokenSource.cancel("Operation canceled due to new request.");
+      // reject(new Error("Time limit exceed"));
+    }, duration);
+  });
+};
+
+fetchWithTimeout("https://jsonplaceholder.typicode.com/todos/1", 1000)
+  .then((resp) => {
+    console.log(resp);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+  // Aborted
+  // error
+
