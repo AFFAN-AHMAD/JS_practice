50aaac6222d4d5f52ceeb69a5a8a18fc77b65d4b
affan <affan.ahmad@superprocure.com>
2024-11-02 22:27:33 +0530

    2024 nov 2: reduce method

--- js practice/asyncSeries/usingReduce.js
+++ js practice/asyncSeries/usingReduce.js
@@ -9,12 +9,38 @@ const asyncTask = async function (params) {
 const promises = [asyncTask(3), asyncTask(2), asyncTask(5), asyncTask(7)];
 
 const asyncSeriesExecuter = (args) => {
-  args.reduce((acc, curr) => {
-    return acc.then(() => {
+  args.reduce((acc, curr, index) => {
+    return acc.then((val1) => {
+      // console.log('index',index);
+      if(index == 1){
+        console.log('val',val1);
+      }
       return curr.then((val) => console.log("val", val));
     });
-  }, Promise.resolve());
+  });
 //   here promise.resolve is used to run the accumulator 
 };
 
 asyncSeriesExecuter(promises);
+
+
+
+
+
+
+
+
+
+
+
+
+let array = [1,2,3,4,5];
+
+array.reduce((acc,curr,index)=>{
+  console.log('index11111',index);
+}, 0);
+
+
+array.forEach((item, index)=>{
+  console.log('indexeachh',index);
+})
