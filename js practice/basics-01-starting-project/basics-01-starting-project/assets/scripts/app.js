64178c4af603cc9392dd8d1ba72e0d03f5063e0c
affan <affan.ahmad@superprocure.com>
2025-06-12 09:38:57 +0530

    12 jun: basic calculator ready

--- js practice/basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
+++ js practice/basics-01-starting-project/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,47 @@
- let defaultResult = 0;
+let defaultResult = 0;
 
- let currentResult= defaultResult;
+let currentResult = defaultResult;
+let calcOutput = 0;
+function clearInput() {
+  userInput.value = "";
+}
+function add() {
+  const enteredValue = userInput.value;
+  if (enteredValue === "") return;
+  currentResult = currentResult + Number(enteredValue);
+  calcOutput = `${calcOutput} + ${enteredValue}`;
+  outputResult(currentResult, calcOutput);
+  clearInput();
+}
 
+function subtract() {
+  const enteredValue = userInput.value;
+  if (enteredValue === "") return;
+  currentResult = currentResult - Number(enteredValue);
+  calcOutput = `${calcOutput} - ${enteredValue}`;
+  outputResult(currentResult, calcOutput);
+  clearInput();
+}
 
- function add() {
-    console.log('currentResult',currentResult);
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, "")
- }
- addBtn.addEventListener('click',add)
- 
+function multiply() {
+  const enteredValue = userInput.value;
+  if (enteredValue === "") return;
+  currentResult = currentResult * Number(enteredValue);
+  calcOutput = `${calcOutput} * ${enteredValue}`;
+  outputResult(currentResult, calcOutput);
+  clearInput();
+}
+
+function divide() {
+  const enteredValue = userInput.value;
+  if (enteredValue === "") return;
+  currentResult = currentResult / Number(enteredValue);
+  calcOutput = `${calcOutput} / ${enteredValue}`;
+  outputResult(currentResult, calcOutput);
+  clearInput();
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
--- js practice/basics-01-starting-project/basics-assignment-2-start/basics-assignment-2-start/assignment.js
+++ js practice/basics-01-starting-project/basics-assignment-2-start/basics-assignment-2-start/assignment.js
@@ -9,6 +9,13 @@ function two(variable){
     alert(variable)
 }
 
+function brandNew(var1, var2, var3){
+    return`${var1} ${var2} ${var3}`;
+}
+
 one();
 two('Hello World from the second function!');
-task3Element.addEventListener('click', one)
+const res = brandNew('Hello', 'World', 'from the third function!');
+alert(res);
+task3Element.addEventListener('click', one);
+
